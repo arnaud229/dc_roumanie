cf375eca3561d59b1b5e1c262059a629c76c7f47
DESKTOP-23GHN7Q\GHBB <lokoelido@gmail.com>
2024-10-09 04:26:07 +0100

    dashboard-user

--- src/app/dashboard-user/dashboard-user.component.ts
+++ src/app/dashboard-user/dashboard-user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+import { LocalstorageService } from '../services/localStorage/localStorage.service';
 
 @Component({
   selector: 'app-dashboard-user',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard-user.component.scss']
 })
 export class DashboardUserComponent {
+
+  selecter = 0;
+
+
+  constructor(
+    private router: Router,
+    public authService: AuthService,
+    public localstorageService: LocalstorageService,
+  ) {}
+
+
+  ngOnInit() {
+   
+  }
+
+
+
+  selected(index: number) {
+    if (index == 0) {
+
+      this.selecter = 0 ;
+      
+    } else if (index == 1) {
+
+       this.selecter = 1;
+
+    } else if (index == 2) {
+
+      this.selecter = 2 ;
+      
+    }
+
+  }
+
+  logOut() {
+
+    this.localstorageService.logout()
+
+  }
 
 }
